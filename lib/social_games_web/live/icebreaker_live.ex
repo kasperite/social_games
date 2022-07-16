8e97ec916c661a66778041387e32a68dc9bc1319
Lukas Nguyen <lukas.nguyen9288@gmail.com>
2022-07-16 20:14:59 +1200

    WIP

--- lib/social_games_web/live/icebreaker_live.ex
+++ lib/social_games_web/live/icebreaker_live.ex
@@ -1,13 +1,40 @@
 defmodule SocialGamesWeb.IcebreakerLive do
   use SocialGamesWeb, :live_view
 
-  def render(assigns) do
-    ~L"""
-      <h1>Icebreaker</h1>
-    """
+  @topic "auction"
+
+  def mount(_params, _session, socket) do
+    SocialGamesWeb.Endpoint.subscribe(@topic)
+    {:ok, assign(socket, :val, 0)}
   end
 
-  def mount(params, session, socket) do
-    {:ok, assign(socket, :raised, 0)}
+  def handle_event("inc", _, socket) do
+    # val = &(&1 + 1)
+    val = socket.assigns.val + 1
+    SocialGamesWeb.Endpoint.broadcast_from(self(), @topic, "changed", val)
+    # {:noreply, update(socket, :val, val)}
+    {:noreply, assign(socket, :val, val)}
+  end
+
+  def handle_event("dec", _, socket) do
+    # val = &(&1 - 1)
+    val = socket.assigns.val - 1
+    SocialGamesWeb.Endpoint.broadcast_from(self(), @topic, "changed", val)
+    # {:noreply, update(socket, :val, val)}
+    {:noreply, assign(socket, :val, val)}
+  end
+
+  def handle_info(%{topic: @topic, payload: val}, socket) do
+    {:noreply, assign(socket, :val, val)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <h1>The count is: <%= @val %></h1>
+      <button phx-click="dec">-</button>
+      <button phx-click="inc">+</button>
+    </div>
+    """
   end
 end
